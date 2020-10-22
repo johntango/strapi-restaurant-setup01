8eee359c4305386320d906db4ca78b2e53fbdde0
John Williams <jrwtango@gmail.com>
2020-10-22 10:08:42 -0400

    first commit

---
+++ restaurant/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6ba28c2dd0888685f368a9f5192f385f'),
+    },
+  },
+});
---
+++ restaurant/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ad7d043e-af19-4b78-bfd7-5795334dcd33'
+};
